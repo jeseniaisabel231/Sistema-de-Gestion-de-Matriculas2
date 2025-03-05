10025b02b6c7db939f48d2a28a86e2fb6082f378
jeseniaisabel231 <jesenia.pazto@epn.edu.ec>
2025-03-05 11:41:21 -0500

    cambio final

--- src/app/components/formulario.component.ts
+++ src/app/components/formulario.component.ts
@@ -9,6 +9,7 @@ import {
   SimpleChanges,
   output,
   computed,
+  signal,
 } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
@@ -84,10 +85,26 @@ export type Actions = 'Registrar' | 'Actualizar' | 'Visualizar';
           Cancelar
         </button>
         <button
-          class="bg-[#181818] rounded-[10px] p-2 text-white px-5 font-normal"
+          class="bg-[#181818] rounded-[10px] p-2 text-white px-5 font-normal disabled:opacity-85"
           (click)="onSubmit()"
+          [disabled]="carga()"
         >
+          @if(carga()){
+          <svg
+            class="animate-spin fill-[#ffffff] mx-auto"
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+          >
+            <path
+              d="M480-60.78q-86.52 0-162.9-32.96-76.37-32.95-133.39-89.97T93.74-317.1Q60.78-393.48 60.78-480q0-87.04 32.95-163.06 32.95-76.03 89.96-133.18t133.4-90.07q76.39-32.91 162.91-32.91 22.09 0 37.54 15.46Q533-868.3 533-846.22q0 22.09-15.46 37.55-15.45 15.45-37.54 15.45-130.18 0-221.7 91.52t-91.52 221.69q0 130.18 91.52 221.71 91.52 91.52 221.69 91.52 130.18 0 221.71-91.52 91.52-91.52 91.52-221.7 0-22.09 15.45-37.54Q824.13-533 846.22-533q22.08 0 37.54 15.46 15.46 15.45 15.46 37.54 0 86.52-32.95 162.92t-89.96 133.44q-57.01 57.03-133.1 89.95Q567.12-60.78 480-60.78"
+            />
+          </svg>
+          }@else{
+
           {{ acciones() }}
+          }
         </button>
       </div>
       }
@@ -101,6 +118,9 @@ export class Formulario {
   public mostrarModal = model<boolean>(false);
   public titulo = input.required<TituloForms>();
   public acciones = input.required<Actions>();
+
+  //variable de carga
+  public carga = signal<boolean>(false);
 
   //variable para emitir el cambio
   public cambioEmitir = output<any>();
@@ -135,7 +155,7 @@ export class Formulario {
       codigo: 'text',
       nombre: 'text',
       ubicacion: 'text',
-      capacidad: 'text',
+      capacidad: 'number',
       descripcion: 'text',
     },
     reserva: {
@@ -168,6 +188,7 @@ export class Formulario {
       alert('Formulario inválido');
       return;
     }
+    this.carga.set(true);
     //fun cion para crear un registro
     if (this.acciones() === 'Registrar') {
       this.servicioRegistrar()
@@ -188,6 +209,9 @@ export class Formulario {
               alert(error.response); //error.response y este contiene el mensaje
             }
           },
+        })
+        .add(() => {
+          this.carga.set(false);
         });
     } else {
       //funcion para actualizar registro
@@ -203,8 +227,10 @@ export class Formulario {
           error: ({ error }: { error: any }) => {
             alert(error.response);
           },
+        })
+        .add(() => {
+          this.carga.set(false);
         });
     }
-    
   }
 }
